342ecba5f29640076ef6a37d62d296e5e636ed34
ysshen <hustshenyang@126.com>
2021-11-10 17:03:45 +0800

    feat：添加烧录脚本

---
+++ flash.bat
@@ -0,0 +1,1 @@
+esptool.py.exe -p %1 -b 460800 --before default_reset --after hard_reset --chip esp32 write_flash --flash_mode dio --flash_mode dio --flash_size detect --flash_freq 80m 0x1000 build/bootloader/bootloader.bin 0x8000 build/partition_table/partition-table.bin 0x10000 build/hello-world.bin
